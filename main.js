f606a7141d6a8a1339e097425314f0e09d4ab634
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-08-21 15:37:51 +0200

    button fix

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ const mySiema = new Siema();
 
 document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
 document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
