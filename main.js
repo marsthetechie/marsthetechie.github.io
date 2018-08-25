c8e887d7096b8ea6abbf52b868ba4b4add4eaa7a
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-08-25 20:21:52 +0200

    add buttons for social networks

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ const mySiema = new Siema();
 
 document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
 document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
