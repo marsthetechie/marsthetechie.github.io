1f72cb40bbfe192c1dbb7af9e15240ae939ca799
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-09-09 18:30:56 +0200

    style project section

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ const mySiema = new Siema();
 document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
 document.querySelector('.next').addEventListener('click', () => mySiema.next());
 
+const mySiema2 = new Siema({
+    selector: '.mySiema'
+});
+
+document.querySelector('.prev2').addEventListener('click', () => mySiema2.prev());
+document.querySelector('.next2').addEventListener('click', () => mySiema2.next());
